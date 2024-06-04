45f3c0f9147d76b255e2079e4227a232687b66c1
linus Feldkamp <githubai@roboticsheep.de>
2024-06-04 19:41:57 +0200

    typo

--- main.py
+++ main.py
@@ -13,10 +13,11 @@ from dotenv import load_dotenv
 # Load environment variables from .env file
 load_dotenv()
 
-llm = ChatOpenAI(model_name="crewai-llama3:8b", temperature=0.7)
+llm = ChatOpenAI(model_name="crewai-llama3:8b", temperature=0.8)
 
 debate_topic = "Should Data Scientist write backend and API code as well?"
-output =llm.predict_messages([HumanMessage(content="I wish to have a debate on {}. What would be the two most likely fighting sides, output just the names separated by a comma, nothing else".format(debate_topic))])
+output =llm.predict_messages([HumanMessage(content="I wish to have a debate on {}.Do not write anything exept the names of the two fighting parties.".format(debate_topic))])
 output_parser = CommaSeparatedListOutputParser()
 classes = output_parser.parse(output.content)
+#  Not really working with llama3
 print(classes)
