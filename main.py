a3098daae2702af544d6c493800eb70590d5506b
linus Feldkamp <githubai@roboticsheep.de>
2024-06-04 19:33:59 +0200

    fix: change backend openai interface

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from typing import Dict, TypedDict, Optional
 from langchain_core.output_parsers import CommaSeparatedListOutputParser
 from langgraph.graph import StateGraph,END
 from langchain_openai import ChatOpenAI
+from langchain.schema import HumanMessage
+from langchain.prompts import SystemMessagePromptTemplate
 import random
 import time
 import os
@@ -14,8 +16,7 @@ load_dotenv()
 llm = ChatOpenAI(model_name="crewai-llama3:8b", temperature=0.7)
 
 debate_topic = "Should Data Scientist write backend and API code as well?"
-
-output =llm("I which to have a debate on {}. What would be the fighting sides. Output just the names")
+output =llm.predict_messages([HumanMessage(content="I which to have a debate on {}. What would be the two most likely fighting sides, output just the names separated by a comma, nothing else".format(debate_topic))])
 output_parser = CommaSeparatedListOutputParser()
-classes = output_parser.parse(output)
+classes = output_parser.parse(output.content)
 print(classes)
